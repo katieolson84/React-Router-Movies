9aea13fb806f1fd52a04a6b5d0f60b6a8a348257
Katie Olson <katieolson@katies-air.lan>
2020-11-10 09:24:27 -0700

    cleaned up comment code

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,15 @@ import Movie from './Movies/Movie'
 
 
 export default function App () {
-  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5000/api/movies') 
         .then(response => {
           // console.log(response);
           setMovieList(response.data)
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
         })
         .catch(error => {
           console.error('Server Error', error);
